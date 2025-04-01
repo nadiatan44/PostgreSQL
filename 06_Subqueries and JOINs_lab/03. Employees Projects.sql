7f6da35ef65a7fab176af43d3f55b6f03667ac1b
Nadezhda Taneva <157598327+nadiatan44@users.noreply.github.com>
2025-04-01 10:46:39 +0200

    Add files via upload

---
+++ 06_Subqueries and JOINs_lab/03. Employees Projects.sql
@@ -0,0 +1,12 @@
+SELECT
+	e.employee_id,
+	CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+	p.project_id,
+	p.name AS project_name
+FROM employees AS e
+	JOIN employees_projects as ep
+		USING(employee_id)
+			JOIN projects AS p
+				USING(project_id)
+WHERE p.project_id = 1
+	
---
+++ 06_Subqueries and JOINs_lab/04. Higher Salary.sql
@@ -0,0 +1,5 @@
+SELECT
+    count(*)
+FROM employees
+WHERE
+    salary > (SELECT avg(salary) FROM employees)
